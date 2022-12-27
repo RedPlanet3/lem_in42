4f6ec2ddaf09cd79aa148a371cd6f64c707d9bce
Elena Pryakhina <elena@MacBook-Air-Elena.local>
2022-12-27 15:24:49 +0300

    color)

--- srcs/ft_print_func.c
+++ srcs/ft_print_func.c
@@ -15,7 +15,9 @@ void ft_print_matrix(char **matrix)
         ft_putstr_fd("\t", 1);
         for (int j = 0; j < (int)g_vars.number_of_rooms; j++)
         {
+            ft_putstr_fd("\033[1;32m",1);
             ft_putchar_fd(matrix[i][j],1);
+            ft_putstr_fd("\033[0m",1);
             ft_putstr_fd("\t", 1);
         }
         ft_putstr_fd("\n",1);
@@ -40,10 +42,17 @@ void ft_print_matrix(char **matrix)
 //     ft_putstr_fd("\n",1);
 // }
 
+void color_print()
+{
+
+}
+
 void ft_print_ways(t_fin_ways **g_fin_ways)
 {
-	ft_putstr_fd("nomber of ways: ",1);
+	ft_putstr_fd("\033[1;34mnomber of ways:\033[0m",1);
+    ft_putstr_fd("\033[1;35m",1);
     ft_putstr_fd(ft_itoa(g_vars.number_of_ways),1);
+    ft_putstr_fd("\033[0m",1);
     ft_putstr_fd("\n",1); 
 	t_fin_ways *ways = *g_fin_ways;
 	while (ways)
@@ -53,10 +62,11 @@ void ft_print_ways(t_fin_ways **g_fin_ways)
 		{
             ft_putstr_fd(g_vars.list_room[ways->step_index[i]]->name,1);
             if (i != ways->length_way - 1)
-                ft_putstr_fd("=>", 1);
+                ft_putstr_fd("\033[0;31m=>\033[0m",1);
         }
 		ft_putstr_fd("\n",1);
 		ways = ways->next;
 	}
     ft_putstr_fd("\n",1);
 }
+
